686ca1bda85cc3f31f31244a5a16ee3a40c70280
Abe Malla <abe.malla8@gmail.com>
2020-12-18 16:32:05 -0800

    Replaced neural network with sklearn nn

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,50 +1,50 @@
 import numpy as np
+import pandas as pd
 
-# Included to get reproducible outputs
-np.random.seed(0)
-
-
-class Layer:
-    def __init__(self, batchCount: int, neuronsCount: int):
-        self.batchCount = batchCount
-        self.weights = np.random.rand(batchCount, neuronsCount)
-        self.biases = np.zeros((1, neuronsCount))
-
-    def forward(self, inputs: np.array) -> float:
-        self.outputs = np.dot(inputs, self.weights) + self.biases
-        self.outputs = [Layer.sigmoid(i) for i in self.outputs]
-
-    def getMSE(self, correct: np.array) -> float:
-        return np.square(np.subtract(self.output, correct)).mean()
-
-    @staticmethod
-    def sigmoid(x: np.array) -> float:
-        return 1 / (1 + np.exp(-x))
-
-    @staticmethod
-    def sigmoidDeriv(x: np.array):
-        return Layer.sigmoid(x) * (1 - Layer.sigmoid(x))
-
-
-class NeuralNetwork:
-    def __init__(self, batchCount: int):
-        # 12 possible inputs
-        self.inputLayer = Layer(batchCount, 12)
-        self.hiddenLayer1 = Layer(12, 12)
-        self.hiddenLayer2 = Layer(12, 12)
-        # 2 possible outputs
-        self.outputLayer = Layer(12, 2)
-
-    def feedForward(self, batchInputs: np.array):
-        self.inputLayer.forward(batchInputs)
-        self.hiddenLayer1.forward(self.inputLayer.outputs)
-        self.outputLayer.forward(self.hiddenLayer1.outputs)
-        print(self.outputLayer.outputs)
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
 
 
 if __name__ == '__main__':
-    batchCount = 1
-    neuronCount = 12
-    inputs = np.random.rand(batchCount, batchCount)
-    n = NeuralNetwork(batchCount)
-    n.feedForward(inputs)
+    # Ignores a false positive warning relating to copying and combining DataFrames
+    pd.options.mode.chained_assignment = None
+
+    dataColumns = ['Mode', 'Time Signature', 'Acousticness', 'Danceability', 'Energy',
+                   'Instrumentalness', 'Liveness', 'Loudness', 'Speechiness', 'Valence', 'Tempo']
+
+    # Load and concatenate the data
+    likedRapSongs = pd.read_csv('data/likedRapSongs.csv')
+    likedSongs = likedRapSongs[dataColumns]
+    likedSongs['Liked'] = 1
+
+    dislikedSongs = pd.read_csv('data/dislikedSongsHipHop.csv')
+    dislikedSongs = dislikedSongs[dataColumns]
+    dislikedSongs['Liked'] = 0
+
+    allSongs = pd.concat([likedSongs, dislikedSongs])
+    X = allSongs[dataColumns]
+    y = allSongs['Liked']
+
+    # Split the data
+    xTrain, xTest, yTrain, yTest = train_test_split(X, y)
+
+    # Feature scaling
+    scaler = StandardScaler()
+    scaler.fit(xTrain)
+
+    xTrain = scaler.transform(xTrain)
+    xTest = scaler.transform(xTest)
+
+    # Train
+    mlp = MLPClassifier(hidden_layer_sizes=(12, 7),
+                        random_state=1,
+                        activation='tanh',
+                        max_iter=1000,
+                        batch_size=64)
+    mlp.fit(xTrain, yTrain)
+
+    # Test
+    predictions = mlp.predict(xTest)
+    print(accuracy_score(yTest, predictions))
